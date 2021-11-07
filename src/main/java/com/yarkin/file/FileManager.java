590e4eb7354a66bab808c1db8e6c82bd139a9f77
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-07 18:05:20 +0200

    Implement countFiles()

--- src/main/java/com/yarkin/file/FileManager.java
+++ src/main/java/com/yarkin/file/FileManager.java
@@ -1,11 +1,26 @@
 package com.yarkin.file;
 
 import java.io.File;
+import java.nio.file.NotDirectoryException;
 
 public class FileManager {
-    public static int countFiles(String path) {
+    public static int countFiles(String path) throws NotDirectoryException {
         File file = new File(path);
 
-        return 0;
+        if(!file.isDirectory()) {
+            throw new NotDirectoryException("\"" + path + "\"" + " - is not a directory");
+        }
+
+        int totalFiles = 0;
+        for (File f : file.listFiles()) {
+            if(f.isDirectory()) {
+                totalFiles += countFiles(f.getPath());
+            } else {
+                totalFiles++;
+            }
+
+        }
+
+        return totalFiles;
     }
 }
