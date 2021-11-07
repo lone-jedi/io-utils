17661e8b2f38d1887d3e74df806326e41f6ad6b6
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-07 18:04:47 +0200

    Correct wrong expected values & add new test of countFiles() on folder with files

--- src/test/java/com/yarkin/file/FileManagerTest.java
+++ src/test/java/com/yarkin/file/FileManagerTest.java
@@ -8,25 +8,33 @@ import java.nio.file.NotDirectoryException;
 public class FileManagerTest {
 
     @Test
-    public void testCountFilesWithOnlyDirectories() {
-        int expected = 7;
+    public void testCountFilesWithOnlyDirectories() throws NotDirectoryException {
+        int expected = 0;
         int actual = FileManager.countFiles(TestDirectory.ONLY_DIRECTORIES.toString());
 
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testCountFilesWithOnlyFiles() {
-        int expected = 10;
+    public void testCountFilesWithOnlyFiles() throws NotDirectoryException {
+        int expected = 2;
         int actual = FileManager.countFiles(TestDirectory.ONLY_FILES.toString());
 
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testCountFilesInEmptyDirectory() {
+    public void testCountFilesInEmptyDirectory() throws NotDirectoryException {
         int expected = 0;
         int actual = FileManager.countFiles(TestDirectory.EMPTY.toString());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountFiles() throws NotDirectoryException {
+        int expected = 3;
+        int actual = FileManager.countFiles(TestDirectory.WITH_FILES.toString());
 
         assertEquals(expected, actual);
     }
