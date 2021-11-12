2ebe0b4b93efddb20ea52ad97bcb32f734a6318e
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-12 13:32:39 +0200

    Remove unnesessary imports

--- src/test/java/com/yarkin/file/analyzer/FileAnalyzerTest.java
+++ src/test/java/com/yarkin/file/analyzer/FileAnalyzerTest.java
@@ -1,10 +1,9 @@
 package com.yarkin.file.analyzer;
 
-import org.junit.jupiter.api.Assertions;
+import com.yarkin.file.TestFile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
